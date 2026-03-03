b1d1db7f1d6087cb5af5c6b077cbdd78725d622d
lucakele17 <lucakele@gmail.com>
2026-03-03 15:10:55 +0100

    List items in cart

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ let totalPrice = 0;
 function addToCart(product, price) {
   localStorage.setItem(product, price);
   totalPrice += price;
-  listItems();
 }
 
 function removeFromCart(index) {
@@ -13,7 +12,10 @@ function removeFromCart(index) {
 }
 
 function listItems() {
+  const ulElement = document.getElementById("cart");
   for (let i = 0; i < localStorage.length; i++) {
-    console.log(localStorage.key(i));
+    const liElement = document.createElement("li");
+    liElement.innerText = localStorage.key(i);
+    ulElement.appendChild(liElement);
   }
 }
