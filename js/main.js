71dc13abcb025ab1022e8a779d4928bbdf77ad4d
lucakele17 <243143754+lucakele17@users.noreply.github.com>
2026-03-04 15:21:39 +0100

    Add remove button and total price to items in cart

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-let totalPrice = 0;
-
 function addToCart(product, price) {
   localStorage.setItem(product, price);
-  totalPrice += price;
 }
 
 function removeFromCart(index) {
@@ -13,9 +10,35 @@ function removeFromCart(index) {
 
 function listItems() {
   const ulElement = document.getElementById("cart");
+
+  ulElement.innerHTML = "";
+
   for (let i = 0; i < localStorage.length; i++) {
     const liElement = document.createElement("li");
-    liElement.innerText = localStorage.key(i);
+    liElement.innerHTML = `
+      <span class="product-name">${localStorage.key(i)}</span>
+      <button class="remove-button" id="${i}">Remove</button>
+    `;
     ulElement.appendChild(liElement);
   }
+
+  showTotalPrice();
 }
+
+function showTotalPrice() {
+  const priceElement = document.getElementById("total-price");
+  let totalPrice = 0;
+
+  for (let i = 0; i < localStorage.length; i++) {
+    totalPrice += Number(localStorage.getItem(localStorage.key(i)));
+  }
+
+  priceElement.innerText = "Total price: " + totalPrice + " SEK";
+}
+
+document.getElementById("cart").addEventListener("click", function (e) {
+  if (e.target.classList.contains("remove-button")) {
+    const index = e.target.getAttribute("id");
+    removeFromCart(index);
+  }
+});
